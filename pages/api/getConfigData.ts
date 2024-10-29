0cab630612ca737aae54dae7692990447e811b79
jesus <soler98@hotmail.es>
2024-10-29 09:58:37 +0100

    Proyectos separados en ramas.

--- pages/api/getConfigData.ts
+++ pages/api/getConfigData.ts
@@ -35,3 +35,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
+
+//Proyecto separados.
